4ee12b8e7483adc2b9e7ccc2cf3fbe1f524fef56
Simon Demeulemeester <simondemeulemeester@MacBook-Pro.local>
2024-06-20 17:49:21 +0200

    mobile projects section

--- src/components/MobilePojects.jsx
+++ src/components/MobilePojects.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Project from "./ui/Project";
 
 const MobilePojects = () => {
@@ -5,32 +6,32 @@ const MobilePojects = () => {
     {
       title: "Project 1",
       description: "Design & Development",
-      img: "",
+      img: "/assets/webpage1crop.webp",
       right: false,
     },
     {
       title: "Project 2",
       description: "Development",
-      img: "",
+      img: "/assets/webpage2.webp",
       right: true,
     },
     {
       title: "Project 3",
       description: "Design & Development",
-      img: "",
+      img: "/assets/webpage3.webp",
       right: false,
     },
     {
       title: "Project 4",
       description: "Design",
-      img: "",
+      img: "/assets/webpage4.webp",
       right: true,
     },
   ];
 
   return (
     <div>
-      <h1 className="text-mobsubheading uppercase font-semibold mb-[70px]">
+      <h1 className="text-mobsubheading uppercase font-semibold">
         (01) Recent work
       </h1>
       {works.map((work, index) => (
--- src/components/ui/Project.jsx
+++ src/components/ui/Project.jsx
@@ -1,14 +1,28 @@
+"use client";
+
+import Image from "next/image";
+
 const Project = ({ title, description, img, right }) => {
   return (
     <section>
-      <div className="flex flex-col gap-9">
+      <div className="flex flex-col gap-9 my-[75px]">
         <div className={`flex flex-col gap-3 ${right ? "items-end" : ""}`}>
           <h1 className="text-mobwork uppercase font-regular">{title}</h1>
           <p className="text-mobbody capitalize font-medium">{description}</p>
         </div>
-        <div>
-          <img src={img}></img>
+        <div className="flex justify-center">
+          <a href="">
+            <div className="w-[340px] h-[200px] relative rounded-md overflow-hidden shadow-md">
+              <Image
+                src={img}
+                fill
+                className="object-cover object-top rounded-md hover:scale-105 duration-300 ease-out"
+                quality={100}
+              ></Image>
+            </div>
+          </a>
         </div>
+        <div className="w-full h-[1px] bg-txt mt-[40px]"></div>
       </div>
     </section>
   );
